25d4b71a3c75cd3616a422359a6ff88e8b351241
NetscriptStudio <83537037+NetscriptStudio@users.noreply.github.com>
2021-08-30 17:44:19 +0300

    Update WebServer.js

--- WebServer.js
+++ WebServer.js
@@ -1,1 +1,11 @@
+'use strict';
 
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
